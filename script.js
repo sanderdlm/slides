90e7c0c46fc22b72f2e2b4d970568c5dd7b15e43
Dreadnip <sander.delamarche@outlook.com>
2020-02-25 04:05:34 +0100

    Bazinga

---
+++ script.js
@@ -0,0 +1,31 @@
+const sections = document.querySelectorAll('section');
+const last = sections.length;
+let current = 0;
+let target = null;
+
+document.addEventListener('keydown', (event) => {
+	
+	if (event.keyCode === 38) {
+		event.preventDefault();
+
+		//scroll up
+		if (current > 0) {
+			current--;
+		}	
+	}
+
+	if (event.keyCode === 40) {
+		event.preventDefault();
+
+		//scroll down
+		if (current < last - 1) {
+			current++;
+		}
+	}
+
+	target = sections[current];
+
+	if (target) {
+		target.scrollIntoView();
+	}
+});
